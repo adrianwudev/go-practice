2dc2e812aa1baba77ca00613202fa7649a963aa7
adrian <adrian657984@gmail.com>
2022-12-17 15:01:36 +0800

    CLI Task Manager - Create Bolt DB Bucket

--- go.mod
+++ go.mod
@@ -7,7 +7,11 @@ require (
 	golang.org/x/net v0.2.0
 )
 
+require golang.org/x/sys v0.2.0 // indirect
+
 require (
+	github.com/boltdb/bolt v1.3.1
 	github.com/inconshreveable/mousetrap v1.0.1 // indirect
+	github.com/mitchellh/go-homedir v1.1.0
 	github.com/spf13/pflag v1.0.5 // indirect
 )
--- task/main.go
+++ task/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "github.com/adrianwudev/go-practice/task/cmd"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/adrianwudev/go-practice/task/cmd"
+	"github.com/adrianwudev/go-practice/task/db"
+	homedir "github.com/mitchellh/go-homedir"
+)
 
 func main() {
-	cmd.RootCmd.Execute()
+	home, _ := homedir.Dir()
+	dbPath := filepath.Join(home, "tasks.db")
+	must(db.Init(dbPath))
+	must(cmd.RootCmd.Execute())
+}
+
+func must(err error) {
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
 }
